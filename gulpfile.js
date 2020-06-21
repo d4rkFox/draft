2c2161eccb6895d5a629cc8e47dc31b927e7b093
d4rkFox <maks.petri4enko@yandex.ru>
2020-06-21 20:54:00 +0700

    frontend build system version 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp'),
   sass = require('gulp-sass'),
+  cleancss = require("gulp-clean-css"),
   rename = require('gulp-rename'),
   browserSync = require('browser-sync'),
   autoprefixer = require('gulp-autoprefixer'),
@@ -8,16 +9,46 @@ let gulp = require('gulp'),
   cssmin = require('gulp-cssmin'),
   ttf2woff2 = require('gulp-ttf2woff2'),
   ttf2woff = require('gulp-ttf2woff'),
-  include = require('gulp-file-include')
+  include = require('gulp-file-include'),
+  size = require('gulp-filesize'),
+  imagemin = require('gulp-imagemin'),
+  recompress = require('imagemin-jpeg-recompress'),
+  pngquant = require('imagemin-pngquant'),
+  webp = require('gulp-webp'),
+  babel = require("gulp-babel"),
+  sourcemaps = require("gulp-sourcemaps");
 
 
 gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp
+    .src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 8 versions']
     }))
     .pipe(rename({ suffix: '.min' }))
+    .pipe(
+      cleancss({
+        compatibility: "ie8",
+        level: {
+          1: {
+            specialComments: 0,
+            removeEmpty: true,
+            removeWhitespace: true,
+          },
+          2: {
+            mergeMedia: true,
+            removeEmpty: true,
+            removeDuplicateFontRules: true,
+            removeDuplicateMediaBlocks: true,
+            removeDuplicateRules: true,
+            removeUnusedAtRules: true,
+          },
+        },
+      }),
+    )
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({ stream: true }))
 });
@@ -31,6 +62,22 @@ gulp.task('style', function () {
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
     .pipe(gulp.dest('app/css'))
+    .pipe(size())
+});
+
+gulp.task("minjs", function () {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(size())
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min",
+      }),
+    )
+    .pipe(gulp.dest("app/js"))
+    .pipe(size());
 });
 
 gulp.task('script', function () {
@@ -38,9 +85,12 @@ gulp.task('script', function () {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
   ])
+    .pipe(size())
+    .pipe(babel())
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/js'))
+    .pipe(size())
 });
 
 gulp.task('html', function () {
@@ -70,7 +120,7 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('ttf2woff', function () {
-  return gulp.src(['app/fonts/*.ttf'])
+  return gulp.src(['src/fonts/*.ttf'])
     .pipe(ttf2woff())
     .pipe(gulp.dest('app/fonts/'));
 });
@@ -81,13 +131,60 @@ gulp.task('ttf2woff2', function () {
     .pipe(gulp.dest('app/fonts/'));
 });
 
+gulp.task('images', function () {
+  return gulp
+    .src("src/images/**/*.+(png|jpg|jpeg|gif|svg|ico|webp)")
+    .pipe(size())
+    .pipe(
+      imagemin(
+        [
+          recompress({
+            loops: 4, //количество прогонок изображения
+            min: 50, //минимальное качество в процентах
+            max: 95, //максимальное качество в процентах
+            quality: 'high', 
+            use: [pngquant()],
+          }),
+          imagemin.gifsicle(), //тут и ниже всякие плагины для обработки разных типов изображений
+          imagemin.optipng(),
+          imagemin.svgo(),
+        ],
+      ),
+    )
+    .pipe(gulp.dest("app/images/"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      }),
+    )
+    .pipe(size());
+});
+
+gulp.task("webp", function () {
+  return gulp
+    .src("src/images/**/*.+(png|jpg|jpeg|gif|svg|ico|webp)")
+    .pipe(size())
+    .pipe(webp({
+      quality: 95,
+      method: 6,
+    }))
+    .pipe(gulp.dest("app/images"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      }),
+    )
+    .pipe(size())
+});
+
 gulp.task('watch', function () {
   gulp.watch('src/scss/**/*.scss', gulp.parallel('sass'))
-  gulp.watch(['src/*.html','src/components/**/*.html'], gulp.parallel('html'))
-  gulp.watch('src/js/*js', gulp.parallel('js'))
-  gulp.watch('src/fonts/**/*.ttf', gulp.parallel('ttf2woff2', 'ttf2woff'));
+  gulp.watch(['src/*.html', 'src/components/**/*.html'], gulp.parallel('html'))
+  gulp.watch('src/js/*js', gulp.parallel('js', 'minjs'))
+  gulp.watch('src/fonts/**/*.ttf', gulp.parallel('ttf2woff2', 'ttf2woff'))
+  gulp.watch('src/images/**/*.+(png|jpg|jpeg|gif|svg|ico|webp)', gulp.parallel('images'));
 
 })
 
-gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync', 'ttf2woff2', 'ttf2woff'))
+gulp.task('default', gulp.parallel('sass', 'style', 'minjs', 'script', 'watch', 'browser-sync', 'ttf2woff2', 'ttf2woff', 'images'))
 
